8a6b7302017f5b6f43fb184585844f8cf7f7ea39
TheNuclearLemon <killcan@yandex.ru>
2023-10-23 17:54:25 +0500

    Added var testing

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+age = 27
+age = age + 1
+print(age)  # 28
+print(age + 1)  # 29
+age += 1
+print(age)  # 29
+print(type(age))
+print("Your age is " + str(age))
+str(age)
+print ("Your age is " + age)
