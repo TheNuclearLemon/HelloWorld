b1cb9a3c11e9db2fd9772411545a303b2734d398
TheNuclearLemon <killcan@yandex.ru>
2023-10-23 18:17:59 +0500

    Multiple assignments branch added and filled

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-print('HelloWorld')
-print('Тест ниже')
+name = "first_name"
+age = 27
+attractive = True
+print(name)
+print(age)
+print(attractive)
 
-name = "Text_name"
-print("Name if right of this point " + name)
+name, age, attractive = "Name", 28, False
+print(name)
+print(age)
+print(attractive)
 
+Char1 = Char2 = Char3 = Char4 = 30
+print(Char1)
+print(Char2)
+print(Char3)
+print(Char4)
