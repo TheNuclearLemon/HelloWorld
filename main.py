6b7e0d23c613440b83d0e6f35f8177c7f663c09a
TheNuclearLemon <killcan@yandex.ru>
2023-10-24 18:01:10 +0500

    Math functions filled

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('Here is main branch, DNT')
-# master update
+import math
+
+x=-3.14
+y = 10
+z = 1
+print(round(x,2))
+print(math.ceil(x))
+print(math.floor(x))
+print(abs(x))
+print(pow(x,2))
+print(math.sqrt(abs(x)))
+print(max(x,y,z))
+print(min(x,y,z))
