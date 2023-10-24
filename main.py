6f50445b5e754ff0ef603cca54f6ac8078515174
TheNuclearLemon <killcan@yandex.ru>
2023-10-24 17:49:39 +0500

    User input filled

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print('Here is main branch, DNT')
-# master update
+x = input("Write number one: ")
+print("Test str " + x)
+x = int(x) + 1
+print(x)
+
+y = float(input("Write nubmer two: "))
+print(str(type(y))+" "+str(y))
