0b0f3e33e01b84dc645016bb692974de6aea986b
TheNuclearLemon <killcan@yandex.ru>
2023-10-24 19:55:13 +0500

    for loops filled

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print('Here is main branch, DNT')
-# master update
+import time
+#for i in range(10):
+#    print(i)
+
+#for i in range(50,100,5):
+#    print(i)
+
+#for i in "Test_name":
+#    print(i)
+
+for x in range (-10,0):
+    print(abs(x))
+    time.sleep(1)
+print("Done!")
+
+for x in range (10,0,-1):
+    print((x))
+    time.sleep(1)
+print("Done!")
