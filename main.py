1654371088ffe0b7c85575d1c4d79aebabe6bc87
TheNuclearLemon <killcan@yandex.ru>
2023-10-24 19:08:51 +0500

    if_else filled

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('Here is main branch, DNT')
-# master update
+age=int(input("Age: "))
+if age>18:
+    print("Your age is above 18")
+elif age<0:
+    print("Your age below 0")
+elif age==18:
+    print("Your age is 18")
+else:
+    print("Your age is less 18")
+
+print("Your age is 18") if age==18 else print("Your age isn't 18")
+
+print("Your age is 18") if age==18 and age<19 and age>17 else print("Your age isn't 18")
