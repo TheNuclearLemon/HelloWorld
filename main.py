e573b565e35437258d60094d620f2b247417729e
TheNuclearLemon <killcan@yandex.ru>
2023-10-25 18:07:41 +0500

    Var list filled

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print('Here is main branch, DNT')
-# master update
+food = ["pizza", "spaghetti", "water", 20, 25, 40, 20, 30]
+
+# print(type(food))
+# print(food[2])
+
+# food[0]="Not a pizza anymore"
+# print(food[0])
+
+# food.append("ice_cream")
+# food.remove(3)
+# food.remove(4)
+# food.pop()
+# food.insert(1,"cake")
+# for x in food:
+#     x=str(x)
+#     print(x,end=" ")
+#     print(type(x))
+
+#food.sort()
+#food.clear()
+
+for i in food:
+    print(i)
