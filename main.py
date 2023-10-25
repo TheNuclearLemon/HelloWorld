eab35233ab99779a31a0505f5159c23afa374916
TheNuclearLemon <killcan@yandex.ru>
2023-10-25 17:15:58 +0500

    Loop control statements filled

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print('Here is main branch, DNT')
-# master update
+# break
+while True:
+    name=input("Your name: ")
+    if name!="":
+        break
+
+# continue
+phone_number="132-456-7890"
+for i in phone_number:
+    if i == "-":
+        continue
+    print(i,end="")
+
+#pass
+for i in range(1,21):
+    if i ==13:
+        pass
+    else:
+        print(i)
