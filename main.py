53f72d7f335e6c545c980576d3b40e8894a03d83
TheNuclearLemon <killcan@yandex.ru>
2023-10-25 17:08:35 +0500

    Nested loops filled

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print('Here is main branch, DNT')
-# master update
+rows=int(input("How many rows: "))
+columns=int(input("How many columns: "))
+symbol=input("Symbol to use: ")
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol,end="")
+    print()
