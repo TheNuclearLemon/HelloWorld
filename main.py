21bcc27284a39f61b8e0e38c1963fb8e72d1e177
TheNuclearLemon <killcan@yandex.ru>
2023-10-27 13:19:56 +0500

    Tuples filled

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print('Here is main branch, DNT')
-# master update
+# tuple - листы с сортировкой и неизменяемые, для группировки информации
+student=("Student_name",22,"male")
+print(type(student))
+print(student.count("Student_name")) # сколько раз повторяется в тапле
+print(student.index("male")) # какой индекс у значения
+
+for x in student: # вывод содержимого тапла
+    print(x,end=" ")
+print()
+if 22 in student: # проверка есть ли такое содержимое то
+    print("There is 22yo guy")
