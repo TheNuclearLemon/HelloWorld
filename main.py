35c4d142b83f33e940e17b094ee26048b778bc26
TheNuclearLemon <killcan@yandex.ru>
2023-10-27 12:53:49 +0500

    2D lists (multi-demensional lists) filled

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print('Here is main branch, DNT')
-# master update
+drinks=["water","coke","sprite"]
+food=["pizza","sushi","rice"]
+desserts=["cake", "wine"]
+all_food=[drinks,food,desserts]
+print(all_food[1][1])
