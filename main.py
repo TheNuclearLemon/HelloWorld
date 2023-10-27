c874916880177d5fa8a083c6777acf4bf55ba43d
TheNuclearLemon <killcan@yandex.ru>
2023-10-27 16:58:47 +0500

    Index operators filled

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print('Here is main branch, DNT')
-# master update
+name = "test_name"
+print(name)
+# if name[0].islower():
+#     print("1")
+#     name=name.capitalize()
+
+first_name=name[:4].upper()
+last_name=name[5:].upper()
+last_character=name[-2:]
+print(first_name)
+print(last_name)
+print(last_character)
+
+print(name)
