e7df5cf52e2d13ad9f78defdb6c7978823f9b956
TheNuclearLemon <killcan@yandex.ru>
2023-10-28 21:07:34 +0500

    Keywords arguments added

--- main.py
+++ main.py
@@ -5,9 +5,13 @@
 # my_name="Ivan"
 # hello(my_name)
 
-def multiply(n1,n2):
-    result=n1*n2
-    return result # или в одну строку: return n1*n2
-x = multiply(2,3)
-print(x)
-print(multiply(2,4))
+# def multiply(n1,n2):
+#     result=n1*n2
+#     return result # или в одну строку: return n1*n2
+# x = multiply(2,3)
+# print(x)
+# print(multiply(2,4))
+
+def hello(f,s,l):
+    print("Hello "+f + s + l)
+print(hello(l="Last_name ",f="First_name ",s="Second_name "))
