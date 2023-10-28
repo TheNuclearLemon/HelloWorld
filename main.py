519bc5cae964e7aa36980f7f1146e7e4fe66a330
TheNuclearLemon <killcan@yandex.ru>
2023-10-28 21:00:36 +0500

    Added return

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def hello(name):
-    print("Hello "+name)
-#hello("Ivan")
-# name="My name"
-my_name="Ivan"
-hello(my_name)
+# def hello(name):
+#     print("Hello "+name)
+# #hello("Ivan")
+# # name="My name"
+# my_name="Ivan"
+# hello(my_name)
+
+def multiply(n1,n2):
+    result=n1*n2
+    return result # или в одну строку: return n1*n2
+x = multiply(2,3)
+print(x)
+print(multiply(2,4))
