cc77856d8003fd6f3049cbae66f158d06bd1dfe7
TheNuclearLemon <killcan@yandex.ru>
2023-10-30 18:06:35 +0500

    Random filled

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
-print('Here is main branch, DNT')
-# master update
+import random
+x = random.randint(1,6)
+y = random.random()
+print(x,y)
 
-# Закрепление знаний
-kol_uch=int(input("Введите кол-во учеников: ")) # получаем количество учеников
+list=["Rock","Paper","Scissors"]
+z=random.choice(list)
+print(z)
 
-spisok = {} # создаем словарь для переменных
-for x in range(0, kol_uch): # заполняем словарь данными
-    spisok["Student info" + str(x)]=[str(input("Введите имя: ")),int(input(("Введите возраст: "))),str(input("Введите пол: "))]
-for y in spisok.keys(): # превращаем словарь в список, создавая лист с данными
-    Students_info=list(spisok.values())
-
-
-# ch1=str(input("Укажите имя первого ученика: "))
-# ch2=str(input("Укажите имя второго ученика: "))
-# parametr=input("Укажите параметр для сравнения: ")
-# if parametr == "Имя":
-#     if ch1==ch2:
-#         print("Имя одинаковое")
-#     else:
-#         print ("Имя разное")
-# elif parametr == "Возраст":
-#
-
-
-# elif parametr == "Возраст":
-#     for q in Students_info:
-#         for w in range(0,3):
-#             if ch1 == ch2:
-#                 print("Одинаковое имя")
-#             else:
-#                 print("Имена разные")
-
-
-
-
-
-
-# print(spisok)
-# print(Students_info[0])
+cards=[1,2,3,4,5,6,7,8,9,"Jack","Queen","King","Ace"]
+random.shuffle(cards)
+print(cards)
