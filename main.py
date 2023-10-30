3137695b6bf28a74eac26013253e5c6af7f7516d
TheNuclearLemon <killcan@yandex.ru>
2023-10-30 20:23:20 +0500

    File moving filled

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+source="D:\\Projects\\Copy.txt"
+destination="D:\\Projects\\HelloWorld\\Copy.txt"
 
-path = "C:\\Users\\Killcan\\Desktop\\Text.txt"
+try:
+    if os.path.exists(destination):             # проверяет наличие файла, чтобы избежать перезаписи
+        print("There is already a file there")
+    else:
+        os.replace(source,destination)
+        print(source+" was moved")
+except FileNotFoundError:
+    print(source+" was not found")
 
-if os.path.exists(path):
-    print("That location exists")
-    if os.path.isfile(path):    # проверка файл ли
-        print("That is a file")
-    elif os.path.isdir(path):   # проверка папка ли
-        print("That is a directory")
-else:
-    print("That location doesn't exist")
+# чтобы скопировать файл на другой диск - необходимо использовать shutil.move (через него делается copy, а затем delete файла)
