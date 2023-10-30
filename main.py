f1bfed61dc2e57fe0977f919347e1147d737dfac
TheNuclearLemon <killcan@yandex.ru>
2023-10-30 17:26:35 +0500

    Args filled

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
-print('Here is main branch, DNT')
-# master update
-
-# Закрепление знаний
-kol_uch=int(input("Введите кол-во учеников: ")) # получаем количество учеников
-
-spisok = {} # создаем словарь для переменных
-for x in range(0, kol_uch): # заполняем словарь данными
-    spisok["Student info" + str(x)]=[str(input("Введите имя: ")),int(input(("Введите возраст: "))),str(input("Введите пол: "))]
-for y in spisok.keys(): # превращаем словарь в список, создавая лист с данными
-    Students_info=list(spisok.values())
-
-
-# ch1=str(input("Укажите имя первого ученика: "))
-# ch2=str(input("Укажите имя второго ученика: "))
-# parametr=input("Укажите параметр для сравнения: ")
-# if parametr == "Имя":
-#     if ch1==ch2:
-#         print("Имя одинаковое")
-#     else:
-#         print ("Имя разное")
-# elif parametr == "Возраст":
+# def add(num1,num2):
+#     sum=num1+num2
+#     return sum
 #
+# print(add(1,2))
 
+# def add(*args): # *args - позволяет использовать неограниченное количество аргументов, имеет тип tuple; "args" - название, можно использовать любое
+#     sum=0
+#     for i in args:
+#         sum += i
+#     return sum
+#
+# print((add(1,2,3,4)))
 
-# elif parametr == "Возраст":
-#     for q in Students_info:
-#         for w in range(0,3):
-#             if ch1 == ch2:
-#                 print("Одинаковое имя")
-#             else:
-#                 print("Имена разные")
+def add(*args):
+    sum=0
+    args=list(args) # можно изменить тапл на другой тип внутри функции, однако сами аргументы при входе в функцию = тапл
+    args[1]=0
+    for i in args:
+        sum += i
+    return sum
 
-
-
-
-
-
-# print(spisok)
-# print(Students_info[0])
+print(add(1,2,3,4))
