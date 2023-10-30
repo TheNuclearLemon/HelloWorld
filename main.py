04832cb18df2d452b6dd1eae6f9e091ea303e36a
TheNuclearLemon <killcan@yandex.ru>
2023-10-30 19:35:16 +0500

    File reading filled and Text.txt added to the project

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
-
-path = "C:\\Users\\Killcan\\Desktop\\Text.txt"
-
-if os.path.exists(path):
-    print("That location exists")
-    if os.path.isfile(path):    # проверка файл ли
-        print("That is a file")
-    elif os.path.isdir(path):   # проверка папка ли
-        print("That is a directory")
-else:
-    print("That location doesn't exist")
+# with open('File_path') - можно открыть по указанному пути, либо как ниже - если файл в проекте
+try:
+    with open('Text.txt') as file: # таким способом файл будет закрыт после выполнения
+        print(file.read())
+except FileNotFoundError:
+    print("That file was not found")
