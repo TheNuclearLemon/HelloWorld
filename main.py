465a2c81eee14d6cd64d3559a473defce7a8023e
TheNuclearLemon <killcan@yandex.ru>
2023-10-30 17:35:39 +0500

    Kwargs filled

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-# def add(num1,num2):
-#     sum=num1+num2
-#     return sum
+# def hello(**kwargs):
+#     print("Hello " + kwargs['first'] + " | " + kwargs['middle'] + " | " + kwargs['last'])
 #
-# print(add(1,2))
+# hello(first="First_name",last="Last_name",middle="Middle_name")
 
-# def add(*args): # *args - позволяет использовать неограниченное количество аргументов, имеет тип tuple; "args" - название, можно использовать любое
-#     sum=0
-#     for i in args:
-#         sum += i
-#     return sum
-#
-# print((add(1,2,3,4)))
-
-def add(*args):
-    sum=0
-    args=list(args) # можно изменить тапл на другой тип внутри функции, однако сами аргументы при входе в функцию = тапл
-    args[1]=0
-    for i in args:
-        sum += i
-    return sum
-
-print(add(1,2,3,4))
+def hello(**kwargs):
+    print("Hello",end=" ")
+    for key,value in kwargs.items():
+        print(value, end=" ")
+hello(title="Mister", first="First_name",last="Last_name",middle="Middle_name")
